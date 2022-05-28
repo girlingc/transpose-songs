910e217311e1275592919402b95c87957b7a7cd8
Christian Girling <girling.christian@gmail.com>
2022-05-27 18:21:08 -0700

    Create switch statement for notes

--- transpose.js
+++ transpose.js
@@ -1,3 +1,27 @@
-const transposeNotes = () => {
-  
-}
+const transposeNotes = (note) => {
+  switch (note) {
+    case "A":
+
+    case "A#":
+
+    case "B":
+
+    case "C":
+
+    case "C#":
+
+    case "D":
+
+    case "D#":
+
+    case "E":
+
+    case "F":
+
+    case "F#":
+
+    case "G":
+
+    case "G#":
+  }
+};
